b685d4a94a77366823ee28f91743120f0f6cd2c2
Owen Healy <ellbur@gmail.com>
2011-09-14 22:58:53 -0400

    deps

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -15,7 +15,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
 
   override def libraryDependencies = Set(
       "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
-      "net.liftweb" % "lift-openid_2.9.0" % "2.4-M3", // Why no work??
+      "net.liftweb" % "lift-openid_2.9.0" % "2.4-M3",
       "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
       "junit" % "junit" % "4.5" % "test->default",
       "org.scala-tools.testing" %% "specs" % "1.6.8" % "test->default",
